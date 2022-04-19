e089478e23f5801558456a6677066aef1c286cab
ruby-finance <103977440+ruby-finance@users.noreply.github.com>
2022-04-19 11:51:36 +0900

    added contracts

---
+++ contracts/interfaces/ITShareRewardPool.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+interface IRShareRewardPool {
+  function deposit(uint256 _pid, uint256 _amount) external;
+
+  function withdraw(uint256 _pid, uint256 _amount) external;
+
+  function pendingShare(uint256 _pid, address _user)
+    external
+    view
+    returns (uint256);
+
+  function userInfo(uint256 _pid, address _user)
+    external
+    view
+    returns (uint256 amount, uint256 rewardDebt);
+}
